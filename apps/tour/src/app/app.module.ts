d867cfe31d8e53783a3186407487410eafe48a62
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-06 14:40:57 -0300

    feat: adds angular material

--- apps/tour/src/app/app.module.ts
+++ apps/tour/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: 'tour',
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'tour',
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
