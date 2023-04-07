4fb4cffca454e0950b81a5b378ecbc78312f31d4
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-07 16:01:25 -0300

    feat: ads admin portal chart
    - adds ng2-charts lib
    - populates charts for admin

--- libs/admin/src/lib/admin.component.ts
+++ libs/admin/src/lib/admin.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PoiActions, PoiEntity, PoiSelectors } from '@packt/poi';
+import { Subscription } from 'rxjs';
+import { AdminService } from './admin.service';
+// import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'packt-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
+  public data: any;
 
-  constructor() { }
+  private subscription: Subscription | undefined;
+
+  constructor(private store: Store, private adminService: AdminService) {}
 
   ngOnInit(): void {
+    this.subscription = this.store
+      .select(PoiSelectors.getAllPoi)
+      .subscribe((pois) => this.buildChart(pois));
+    this.store.dispatch(PoiActions.init());
   }
 
+  private buildChart(pois: PoiEntity[]) {
+    const labels = pois.map((poi) => poi.name);
+    this.data = {
+      chartType: 'pie',
+      labels: labels,
+      datasets: [
+        {
+          data: this.adminService.getStatistics(pois),
+        },
+      ],
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
--- libs/admin/src/lib/admin.module.ts
+++ libs/admin/src/lib/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { RouterModule } from '@angular/router';
+import { PoiModule } from '@packt/poi';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
@@ -12,6 +14,8 @@ import { RouterModule } from '@angular/router';
         component: AdminComponent,
       },
     ]),
+    PoiModule,
+    NgChartsModule,
   ],
   declarations: [AdminComponent],
 })
